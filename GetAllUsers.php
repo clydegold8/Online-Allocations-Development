91662785d7f48bb7311ac3b6617bbc05d115915d
Clyde Ian Acaso <ian.clyde24@gmail.com>
2016-08-05 11:56:51 +0800

    Added All Users page :hammer:

--- GetAllUsers.php
+++ GetAllUsers.php
@@ -7,4 +7,42 @@
  */
 /* Template Name: GetAllUsers */
 get_header();
+
+$aUsers = get_users();
+
+
+foreach ( $aUsers as $user ) {
+//	var_dump( $user->roles[0]);
+}
+
 ?>
+<div class="row">
+	<div class="col-md-12 text-left">
+		<h1>User List</h1>
+	</div>
+	<div class="col-md-12">
+		<table class="table table-bordered table-hover table-responsive">
+			<thead>
+			<th class="text-center"><b>User Id</b></th>
+			<th class="text-center"><b>Username</b></th>
+			<th class="text-center"><b>Email</b></th>
+			<th class="text-center"><b>Date Registered</b></th>
+			<th class="text-center"><b>Role</b></th>
+			</thead>
+			<tbody>
+			<?php
+			foreach ( $aUsers as $user ) {
+				echo '<tr class="' . ( $user->roles[0] == "administrator" ? 'info' : 'warning' ) . '">';
+				echo '<td>' . esc_html( $user->data->ID ) . '</td>';
+				echo '<td>' . esc_html( ucfirst( $user->data->user_login ) ) . '</td>';
+				echo '<td>' . esc_html( $user->data->user_email ) . '</td>';
+				echo '<td>' . esc_html( $user->data->user_registered ) . '</td>';
+				echo '<td>' . esc_html( ucfirst( $user->roles[0] ) ) . '</td>';
+				echo '<tr>';
+			}
+			?>
+			</tbody>
+		</table>
+	</div>
+</div>
+
